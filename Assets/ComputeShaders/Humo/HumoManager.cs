516a6a40f9766af28c12ac766cd6a5e17d67e285
Victor Blas <victor.blas@enti.cat>
2022-06-05 22:23:00 +0200

    Humo Transparent

--- Assets/ComputeShaders/Humo/HumoManager.cs
+++ Assets/ComputeShaders/Humo/HumoManager.cs
@@ -7,6 +7,7 @@ public class HumoManager : MonoBehaviour
     public Camera mainCamera;
     public GameObject humoPrefab;
     public int humoCount = 100;
+    public float humoMaxHeight = 10;
 
     [HideInInspector] public List<GameObject> humoList;
 
@@ -17,19 +18,31 @@ public class HumoManager : MonoBehaviour
     public struct GameObjectInfo
     {
         public Vector3 position;
-
+        public float speed;
         public static int GetStructSize()
         {
             //Los vec3 son 3 floats, pues neceistamos el total de floats en la struct
-            return sizeof(float) * 3;
+            return sizeof(float) * 4;
         }
     }
 
     void Start()
     {
         for (int i = 0; i < humoCount; i++) {
+
             GameObject humo = Instantiate(humoPrefab, this.transform);
-            humo.transform.position = this.transform.position;
+
+            humo.transform.position = new Vector3(
+                this.transform.position.x + Random.Range(-0.5f, 0.5f),
+                this.transform.position.y + Random.Range(0, humoMaxHeight),
+                this.transform.position.z + Random.Range(-0.5f, 0.5f));
+
+            float randScale = Random.Range(0.01f, 0.2f);
+            humo.transform.localScale = new Vector3(
+                randScale, 
+                0.1f,
+                randScale);
+
             humoList.Add(humo);
         }
 
@@ -39,6 +52,7 @@ public class HumoManager : MonoBehaviour
         for (int i = 0; i < humoList.Count; i++)
         {
             shaderData[i].position = humoList[i].transform.position;
+            shaderData[i].speed = Random.Range(0.005f, 0.02f);
         }
 
         shaderBuffer.SetData(shaderData);
@@ -49,7 +63,10 @@ public class HumoManager : MonoBehaviour
         int kernelHandle = shader.FindKernel("CSHumo");
 
         shader.SetBuffer(kernelHandle, "BoidsResult", shaderBuffer);
-        shader.SetInt("boidCount", humoList.Count);
+        shader.SetFloat("originX", this.transform.position.x);
+        shader.SetFloat("originY", this.transform.position.y);
+        shader.SetFloat("originZ", this.transform.position.z);
+        shader.SetFloat("maxHeight", humoMaxHeight);
 
         //cada 128 gaviotas necesitamos un threadgroup mas
         int threadGroups = Mathf.CeilToInt(humoList.Count / 128.0f);
